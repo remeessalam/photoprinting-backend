19493bcf9faa9367ecbef664d9426ffbfd6970f3
remeessalam <remeesreme4u@gmail.com>
2024-11-30 12:36:37 +0530

    refresh vercel

--- routes/cart.js
+++ routes/cart.js
@@ -332,7 +332,7 @@ router.post(
             if (res.statusCode === 200) {
               res.pipe(cloudinaryUploadStream); // Stream directly to Cloudinary
             } else {
-              console.log(res.statusCode, "htisisereroer");
+              // console.log(res.statusCode, "htisisereroer");
               reject(
                 new Error(`PhotoRoom API failed with status ${res.statusCode}`)
               );
