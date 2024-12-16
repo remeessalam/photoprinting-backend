c53f258d0cfd06ad7d10c1070fb319d50b9abe1f
Akash1419 <samakash153@gmail.com>
2024-12-17 04:33:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,6 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-app.use(
-  cors({
-    origin: "*", // Allow only your frontend origin
-  })
-);
-
 // Error handling middleware
 app.use((err, req, res, next) => {
   if (err.type === "entity.too.large") {
