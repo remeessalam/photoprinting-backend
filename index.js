cb30230c53519d7c9d953bfd402797ceef74aaaf
Senthilnathan <senthilnathan@ailyasa.net>
2024-12-17 12:59:11 +0530

    changes in cors

--- index.js
+++ index.js
@@ -10,19 +10,17 @@ require("dotenv").config();
 app.use(express.json());
 
 app.use(bodyParser.json());
-app.use(cors());
+const corsOptions = {
+  origin: "https://copymudralanka-react.vercel.app", // Replace with your frontend domain
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // Allow cookies if needed
+};
+
+app.use(cors(corsOptions)); // Use CORS with options
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  if (err.type === "entity.too.large") {
-    res.status(413).json({ error: "Payload too large!" });
-  } else {
-    next(err);
-  }
-});
 
 app.get("/", (req, res) => {
   res.send("Hello from the server!");
